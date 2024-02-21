dce2ae21317cf73b76656bca9051da524827ec29
Muneer shafi <zmzahidmuneer@gmail.com>
2024-02-21 11:25:44 +0530

    adding tests

--- tests/CharacterizationTest.php
+++ tests/CharacterizationTest.php
@@ -2,8 +2,38 @@
 
 namespace Gounsch\Test;
 
+use Gounsch\Role;
+use Gounsch\User;
 use PHPUnit\Framework\TestCase;
 
 class CharacterizationTest extends TestCase
 {
+    public function test_user_has_role_admin()
+    {
+
+        $email = 'admin@test';
+        $user = User::create($email);
+        $role = Role::create($user);
+        self::assertNotNull($user->getUser());
+        self::assertEquals($role->role(),'ROLE_ADMIN');
+    }
+
+    public function test_user_has_role_unknown()
+    {
+
+        $email = 'abc';
+        $user = User::create($email);
+        $role = Role::create($user);
+        self::assertNotNull($user->getUser());
+        self::assertEquals($role->role(),'ROLE_UNKNOWN');
+    }
+
+    public function test_user_has_role_USER()
+    {
+
+        $email = 'abc@test';
+        $user = User::create($email);
+        $role = Role::create($user);
+        self::assertEquals($role->role(),'ROLE_USER');
+    }
 }
